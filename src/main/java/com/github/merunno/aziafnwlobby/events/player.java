7bad5b334b0aa6b3b00fd9dcf11b6c93375b1126
merunno <merunno92@gmail.com>
2021-11-25 00:51:29 +0900

    [Fix] 新規参加者なのにロビーへTPしない問題の修正

--- src/main/java/com/github/merunno/aziafnwlobby/events/player.java
+++ src/main/java/com/github/merunno/aziafnwlobby/events/player.java
@@ -1,12 +1,12 @@
 package com.github.merunno.aziafnwlobby.events;
 
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.Objects;
@@ -19,7 +19,10 @@ public class player implements Listener {
         World lobby = Bukkit.getServer().getWorld("lobby");
         Location point = Objects.requireNonNull(lobby).getSpawnLocation();
         if(player.hasPermission("afnw.op.commands")) return;
-        player.hasPlayedBefore();
+        if(!player.hasPlayedBefore()) {
+            player.teleport(point);
+            return;
+        }
         player.teleport(point);
     }
 }
