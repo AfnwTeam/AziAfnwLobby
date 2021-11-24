bec5142b4f2838464d56a791bb9e98835f4c345d
merunno <merunno92@gmail.com>
2021-11-24 17:40:57 +0900

    [Fix] 初参加時にロビーにテレポートされない問題の修正

--- src/main/java/com/github/merunno/aziafnwlobby/events/player.java
+++ src/main/java/com/github/merunno/aziafnwlobby/events/player.java
@@ -1,12 +1,12 @@
 package com.github.merunno.aziafnwlobby.events;
 
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityPickupItemEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 import java.util.Objects;
