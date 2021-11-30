4edc68c1c697bbfa3e76fe8a978dcd9449ebbebd
merunno <merunno92@gmail.com>
2021-11-30 17:23:32 +0900

    [Fix] OPじゃないのにメッセージが表示されてしまう問題の修正

--- src/main/java/com/github/merunno/aziafnwlobby/events/player.java
+++ src/main/java/com/github/merunno/aziafnwlobby/events/player.java
@@ -19,9 +19,10 @@ public class player implements Listener {
         Player player = loginEvent.getPlayer();
         World lobby = Bukkit.getServer().getWorld("lobby");
         Location point = Objects.requireNonNull(lobby).getSpawnLocation();
-        if(player.hasPermission("afnw.op.commands")) return;
+        if(player.hasPermission("afnw.op.commands")) {
+            player.sendMessage(ChatColor.AQUA + "[AziAfnwLobby] OPのため、前回と同じ地点からスタートになります。");
+        }
         if(!player.hasPlayedBefore()) {
-            player.sendMessage(ChatColor.AQUA + "[AziAfnwLobby] 前回の再開地点がAfnwワールドでしたが、OPのため、前回と同じ地点からスタートになります。");
             player.teleport(point);
             return;
         }
