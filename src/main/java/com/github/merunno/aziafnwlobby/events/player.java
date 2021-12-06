be651e931b08b04f23070afba6a8ef8b44d0a095
merunno <merunno92@gmail.com>
2021-12-06 15:19:42 +0900

    [change] タイトル・メッセージの送信へのディレイ

--- src/main/java/com/github/merunno/aziafnwlobby/events/player.java
+++ src/main/java/com/github/merunno/aziafnwlobby/events/player.java
@@ -1,5 +1,6 @@
 package com.github.merunno.aziafnwlobby.events;
 
+import com.github.merunno.aziafnwlobby.AziAfnwLobby;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -8,6 +9,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 
 import java.util.Objects;
@@ -20,8 +23,13 @@ public class player implements Listener {
         World lobby = Bukkit.getServer().getWorld("lobby");
         Location point = Objects.requireNonNull(lobby).getSpawnLocation();
         if(player.hasPermission("afnw.op.commands")) {
-            player.sendTitle(ChatColor.AQUA + "オペレーターリスタート", "オペレーターのため、前回と同じ地点からスタートになります。", 3, 60, 1);
-            player.sendMessage(ChatColor.AQUA + "[AziAfnwLobby] オペレーターリスタート機能が発動しました。オペレーターのため、前回と同じ地点からスタートになります。");
+            new BukkitRunnable() {
+                @Override
+                public void run() {
+                    player.sendTitle(ChatColor.YELLOW + "オペレーターリスタート", "オペレーターのため、前回と同じ地点からスタートになります。", 3, 60, 1);
+                    player.sendMessage(ChatColor.YELLOW + "[AziAfnwLobby] オペレーターリスタート機能が発動しました。オペレーターのため、前回と同じ地点からスタートになります。");
+                }
+            }.runTaskLater(JavaPlugin.getPlugin(AziAfnwLobby.class), 20 * 6);
             return;
         }
         player.teleport(point);
